ec0dafa2583622856be0b19a50a153efe7c4982b
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2015-08-11 17:18:36 +0200

    Initializes massinvandringstreamer now, in mainbot

--- mainbot.py
+++ mainbot.py
@@ -1,26 +1,15 @@
 # Import the twython module
 import twythonaccess
-# import time and sys
-import time
 # import fastreplystreamer
-from massinvandring_streamer import MassInvandringStreamer
-# import Thread to be able to run concurrently
-from threading import Thread
-# randint for the tweet interval
-from random import randint
-
+from massinvandring_streamer import MassinvandringStreamer
 
 
 # the main function will be called when this script is called in terminal
 # the bash command "python3 mainbot.py" will call this function
 def main():
-
-    print("main function")
-
-
-
-
-
+    # start the streamer, and detect for instances of massinvandring in all Swedish tweets
+    streamer = MassinvandringStreamer(apikeys.CONSUMER_KEY, apikeys.CONSUMER_SECRET, apikeys.ACCESS_TOKEN, apikeys.ACCESS_TOKEN_SECRET)
+    streamer.statuses.filter(track = "massinvandring", language = "sv")
 
 
 # if called directly (as in "python3 mainbot.py"), then call main() function
