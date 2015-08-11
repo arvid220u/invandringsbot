ed5c86b1417a96f2b60ae7db7eb3ea277990b48d
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2015-08-11 17:04:41 +0200

    Create mainbot.py

---
+++ mainbot.py
@@ -0,0 +1,28 @@
+# Import the twython module
+import twythonaccess
+# import time and sys
+import time
+# import fastreplystreamer
+from massinvandring_streamer import MassInvandringStreamer
+# import Thread to be able to run concurrently
+from threading import Thread
+# randint for the tweet interval
+from random import randint
+
+
+
+# the main function will be called when this script is called in terminal
+# the bash command "python3 mainbot.py" will call this function
+def main():
+
+    print("main function")
+
+
+
+
+
+
+
+# if called directly (as in "python3 mainbot.py"), then call main() function
+if __name__ == "__main__":
+    main()
