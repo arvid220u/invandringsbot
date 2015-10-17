bf051037739c29d9427e8f633ede6b3d89622ac2
Arvid Lunnemark <arvid.lunnemark@gmail.com>
2015-10-17 17:06:50 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+# This file contains the rant array, the screen name of the user, and the trigger word
+
+# The Twitter handle of the bot.
+screen_name = "invandringgarna"
+
+# Each word that contains this trigger word will be replied to.
+# Note that indicated sarcastic uses of the word (in quotes) will trigger a rant.
+trigger_word = "massinvandring"
+
+# The rant to reply to each user that has tweeted the trigger word with.
+# No more than 15 tweets are allowed, due to the rate limits of the Twitter API.
+rant = ["Tänk om du varit född i Syrien. Eller i Afghanistan. Varför inte Irak.",
+        "Du hade fått välja mellan att ta med din familj på en livsfarlig resa till ett bättre liv, eller riskera bli dödad.",
+        "Du hade valt det senare, för du hade älskat din familj och dina barn.",
+        "Du hade med mycket möda lyckats ta dig till Sverige. Puh, barnen lever!",
+        "Så blir du bemött av dig själv, i det som du trodde var det öppna Sverige.",
+        '"Jag hade turen att födas här, och jag är rädd att jag ska få det lite sämre om jag hjälper dig. Tillbaka till döden!"',
+        "Grattis till att i verkligheten vara född i Sverige. Du hade turen som endast 1 på 1250 har.", 
+        "Dela nu med dig av din tur till andra, för de har lika stor rätt till den som du. Tänk om, tänk rätt."]
